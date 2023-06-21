28c7712ede8475bb650eb162bcc05b92cb102a7e
Ypsen860 <beat.angst@initialize.ch>
2023-06-21 14:09:19 +0200

    Reset level

--- Button.gd
+++ Button.gd
@@ -7,6 +7,8 @@ func _on_Button_pressed():
 	get_parent().get_parent().player.position.x = get_viewport().get_mouse_position().x # Accessing individual components of compound variable is faster than assigning Vector2
 	get_parent().get_node("GameOver").visible = false
 	get_parent().get_parent().game_is_running = true
+	get_parent().get_node("Level").text = str(1)
+	$LevelTimer.stop()
 	# You can make Spawner children to manage themselves or manage them from their manager or relative to their manager.
 	var spawner = get_node("/root/World/Spawner") # You can always provide also full path to the node.
 	for i in range(0, spawner.get_child_count()): # In for loop you don't have to set length - 1 to compare against the count, you just provide the bound as it is.
